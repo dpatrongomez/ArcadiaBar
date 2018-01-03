667f35f71a3b8430cce24d2febcb76937ff42b23
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-01-03 23:17:57 +0100

    Add Back Button

--- app/src/main/java/es/dpatrongomez/arcadiabar/CaraCruz.java
+++ app/src/main/java/es/dpatrongomez/arcadiabar/CaraCruz.java
@@ -9,5 +9,6 @@ public class CaraCruz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cara_cruz);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
