eefb1d977c6a5a49dc9e7af3fc3823edcfcb0305
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-02-05 00:34:22 +0100

    Remove unnecessary if

--- app/src/main/java/es/dpatrongomez/arcadiabar/CaraCruz.java
+++ app/src/main/java/es/dpatrongomez/arcadiabar/CaraCruz.java
@@ -35,22 +35,24 @@ public class CaraCruz extends AppCompatActivity {
             public void onClick(View view) {
                 monedalado = r.nextInt(2);
 
+                RotateAnimation rotate = new RotateAnimation(0, 360,
+                        RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.0f);
+                rotate.setDuration(100);
+                moneda.startAnimation(rotate);
+
                 if (monedalado == 0) {
                     moneda.setImageResource(R.drawable.cara);
                     Toast toast = Toast.makeText(CaraCruz.this, "CARA", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER_VERTICAL, 0, 400);
                     toast.show();
-                } else if (monedalado == 1) {
+                } else {
                     moneda.setImageResource(R.drawable.cruz);
                     Toast toast = Toast.makeText(CaraCruz.this, "CRUZ", Toast.LENGTH_SHORT);
                     toast.setGravity(Gravity.CENTER_VERTICAL, 0, 400);
                     toast.show();
                 }
 
-                RotateAnimation rotate = new RotateAnimation(0, 360,
-                        RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(100);
-                moneda.startAnimation(rotate);
+
 
             }
         });
